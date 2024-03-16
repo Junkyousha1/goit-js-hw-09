4b3e9efb02fe79958d13b31b36df1f49dcec2274
Junkyousha1 <fircosmin5828@gmail.com>
2024-03-16 13:23:07 +0200

    Added color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+  const startButton = document.querySelector("[data-start]");
+  const stopButton = document.querySelector("[data-stop]");
+  
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+  
+  const changeBgColor = () => { document.body.style.backgroundColor = getRandomHexColor() }
+
+  stopButton.disabled = true;
+  let id = 0;
+  startButton.addEventListener("click", () => {
+    id = setInterval(changeBgColor, 1000)
+    changeBgColor()
+    startButton.disabled = true;
+    stopButton.disabled = false;
+  })
+  
+  stopButton.addEventListener("click", () => {
+    clearInterval(id)
+    startButton.disabled = false;
+    stopButton.disabled = true;
+  });
