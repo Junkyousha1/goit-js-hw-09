be5e2d5b51bc0dea78b4c91d673e4851c347e8ba
Junkyousha1 <fircosmin5828@gmail.com>
2024-03-16 14:03:26 +0200

    Added timer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,24 +1,27 @@
-  const startButton = document.querySelector("[data-start]");
-  const stopButton = document.querySelector("[data-stop]");
-  
-  function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
-  
-  const changeBgColor = () => { document.body.style.backgroundColor = getRandomHexColor() }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const changeBgColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+stopButton.disabled = true;
+let id = 0;
+startButton.addEventListener('click', () => {
+  id = setInterval(changeBgColor, 1000);
+  changeBgColor();
+  startButton.disabled = true;
+  stopButton.disabled = false;
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(id);
+  startButton.disabled = false;
   stopButton.disabled = true;
-  let id = 0;
-  startButton.addEventListener("click", () => {
-    id = setInterval(changeBgColor, 1000)
-    changeBgColor()
-    startButton.disabled = true;
-    stopButton.disabled = false;
-  })
-  
-  stopButton.addEventListener("click", () => {
-    clearInterval(id)
-    startButton.disabled = false;
-    stopButton.disabled = true;
-  });
+});
